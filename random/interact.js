51c05e0e2af5d67c4e50ab072f189a293e53ea3a
freedmand <freedmand@gmail.com>
2017-01-02 16:24:55 -0800

    First commit of random number picker

---
+++ random/interact.js
@@ -0,0 +1,36 @@
+function submit(number) {
+  var parsed = parseInt(number);
+  if (parsed && parsed >= 1 && parsed <= 10) {
+    var r = new XMLHttpRequest();
+    r.open("POST", "http://demo-gmpvvl.webscript.io/script?number=" + parsed, true);
+    r.onreadystatechange = function () {
+      if (r.readyState != 4 || r.status != 200) return;
+      process(r.responseText);
+    };
+    r.send();
+  }
+}
+
+function process(response) {
+  var split = response.substring(1, response.length - 1).split(',');
+  var result = [];
+  var total = 0;
+  for (var i = 0; i < split.length; i++) {
+    var num = parseInt(split[i].trim(), 10);
+    result.push(num);
+    total += num;
+  }
+  var total_height = 400;
+  var hist = document.getElementById('hist');
+  var children = hist.children;
+  for (var i = 0; i < 10; i++) {
+    var perc = result[i] / total;
+    var height = Math.round(perc * total_height);
+    if (height < 2) {
+      height = 2;
+    }
+    children[i].style.height = '' + height + 'px';
+  }
+  hist.className = 'histogram finished';
+  document.getElementById('results-text').textContent = 'Total votes cast: ' + total;
+}
